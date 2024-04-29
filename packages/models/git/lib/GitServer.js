ee73586a46a25c3f247616405603cf2bb4b96522
haha58 <1461089222@qq.com>
2024-04-29 08:49:22 +0800

    创建GitServer类

---
+++ packages/models/git/lib/GitServer.js
@@ -0,0 +1,34 @@
+function error(methodName){
+  throw new Error(`${methodName} must be implemented`)
+}
+
+class GitServer{
+  constructor(type,token) {
+    this.type=type;
+    this.token=token
+  }
+
+  //创建远程仓库
+  createRepo(){
+    error("createRepo")
+  }
+
+   //创建组织远程仓库
+   createOrgRepo(){
+    error("createOrgRepo")
+  }
+
+  getRemote(){
+    error("getRemote")
+  }
+
+  getUser(){
+    error("getUser")
+  }
+
+  getOrg(){
+    error("getOrg")
+  }
+}
+
+module.exports=GitServer
--- packages/models/git/lib/index.js
+++ packages/models/git/lib/index.js
@@ -3,91 +3,110 @@
 const SimpleGit = require('simple-git');
 const path = require('path');
 const userHome = require('user-home');
-const log=require('@haha-cli-dev/log')
-const inquirer = require('inquirer')
-const {readFile,writeFile}=require('@haha-cli-dev/utils')
-const fse = require('fs-extra')
+const log = require('@haha-cli-dev/log');
+const inquirer = require('inquirer');
+const { readFile, writeFile } = require('@haha-cli-dev/utils');
+const fse = require('fs-extra');
+const Github = require('./Github');
+const Gitee = require('./Gitee');
 
-const DEFAULT_CLI_HOME='.imooc-cli-dev'
-const GIT_SERVER_FILE='.git_server'    //缓存文件
-const GIT_ROOT_DIR='.git'   //文件根目录
-const GITHUB='Github' 
-const GITEE='Gitee'
-const GIT_SERVER_TYPE=[{
-  name:'Github',
-  value:GITHUB
-},{
-  name:'Gitee',
-  value:GITEE
-}]
+const DEFAULT_CLI_HOME = '.imooc-cli-dev';
+const GIT_SERVER_FILE = '.git_server'; //缓存文件
+const GIT_ROOT_DIR = '.git'; //文件根目录
+const GITHUB = 'Github';
+const GITEE = 'Gitee';
+const GIT_SERVER_TYPE = [
+  {
+    name: 'Github',
+    value: GITHUB,
+  },
+  {
+    name: 'Gitee',
+    value: GITEE,
+  },
+];
 class Git {
-  constructor({ name, version, dir },{refreshServer=false}) {
+  constructor({ name, version, dir }, { refreshServer = false }) {
     this.name = name;
     this.version = version;
     this.dir = dir;
     this.git = SimpleGit(dir);
     this.gitServer = null; //此处需要用户选择
     this.homePath = null; //缓存用户主目录
-    this.refreshServer=refreshServer  //强制刷新
+    this.refreshServer = refreshServer; //强制刷新
   }
 
   async prepare() {
     this.checkHomePath(); //检查缓存主目录
-    this.checkGitServer();  //检查用户远程仓库类型
+    this.checkGitServer(); //检查用户远程仓库类型
   }
   async checkHomePath() {
     try {
-      if(!this.homePath){
-        if(process.env.CLI_HOME_PATH){
-          this.homePath=process.env.CLI_HOME_PATH
-        }else{
-          this.homePath=path.resolve(userHome,DEFAULT_CLI_HOME)
+      if (!this.homePath) {
+        if (process.env.CLI_HOME_PATH) {
+          this.homePath = process.env.CLI_HOME_PATH;
+        } else {
+          this.homePath = path.resolve(userHome, DEFAULT_CLI_HOME);
         }
-        log.verbose('this.homePath',this.homePath)
-        await fse.ensureDirSync(this.homePath)
-        if(!await fse.existsSync(this.homePath)){
-          throw new Error('用户主目录获取失败')
+        log.verbose('this.homePath', this.homePath);
+        await fse.ensureDirSync(this.homePath);
+        if (!(await fse.existsSync(this.homePath))) {
+          throw new Error('用户主目录获取失败');
         }
       }
     } catch (error) {
-      log.error(error.message)
+      log.error(error.message);
     }
   }
 
-  async checkGitServer(){
-    const gitServerPath=this.createPath(GIT_SERVER_FILE)
-    console.log("gitServerPath",gitServerPath)
-    let gitServer=readFile(gitServerPath)
-    console.log("gitServer",gitServer)
-    if(!gitServer||this.refreshServer){
-        gitServer= (await inquirer.prompt({
-        type: 'list',
-        message: '请选择您想要托管的Git平台',
-        name: 'gitServer',
-        default:GITHUB,
-        choices:GIT_SERVER_TYPE
-      })).gitServer
-      console.log("gitServer",gitServer)
-      writeFile(gitServerPath,gitServer)
-      log.success('git server写入成功',`${gitServer} ---> ${gitServerPath}`)
-    }else{
-      log.success('git server获取成功',gitServer)
+  async checkGitServer() {
+    try {
+      const gitServerPath = this.createPath(GIT_SERVER_FILE);
+      console.log('gitServerPath', gitServerPath);
+      let gitServer = readFile(gitServerPath);
+      console.log('gitServer', gitServer);
+      if (!gitServer || this.refreshServer) {
+        gitServer = (
+          await inquirer.prompt({
+            type: 'list',
+            message: '请选择您想要托管的Git平台',
+            name: 'gitServer',
+            default: GITHUB,
+            choices: GIT_SERVER_TYPE,
+          })
+        ).gitServer;
+        console.log('gitServer', gitServer);
+        writeFile(gitServerPath, gitServer);
+        log.success('git server写入成功', `${gitServer} ---> ${gitServerPath}`);
+      } else {
+        log.success('git server获取成功', gitServer);
+      }
+      this.gitServer = this.createGitServer(gitServer);
+      console.log('this.gitServer', this.gitServer);
+      if (!this.gitServer) {
+        throw new Error('GitServer 初始化失败');
+      }
+    } catch (error) {
+      log.error(error.message);
     }
-    this.gitServer=this.createGitServer(gitServer)
   }
 
-  createGitServer(gitServer){
+  createGitServer(gitServer) {
+    if (gitServer === GITHUB) {
+      return new Github();
+    } else if (gitServer === GITEE) {
+      return new Gitee();
+    }
+    return null;
+  }
 
+  createPath(file) {
+    const rootDir = path.resolve(this.homePath, GIT_ROOT_DIR);
+    const filePath = path.resolve(rootDir, file);
+    fse.ensureDirSync(rootDir);
+    return filePath;
   }
-  
-  createPath(file){
-    const rootDir=path.resolve(this.homePath,GIT_ROOT_DIR)
-    const filePath=path.resolve(rootDir,file)
-    fse.ensureDirSync(rootDir)
-    return filePath
-  }
-  init() {
-  }
+  init() {}
 }
 
 module.exports = Git;
