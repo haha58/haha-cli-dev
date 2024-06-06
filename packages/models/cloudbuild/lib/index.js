e08842863681a1ee9d18c5a8e4862f88258cb9fc
haha58 <1461089222@qq.com>
2024-06-06 21:26:13 +0800

    WebSocket超时自动断开连接逻辑开发

--- packages/models/cloudbuild/lib/index.js
+++ packages/models/cloudbuild/lib/index.js
@@ -1,16 +1,24 @@
 'use strict';
 
 const io = require('socket.io-client')
+const log=require('@haha-cli-dev/log')
 
-const TIME_OUT=5*60
+const TIME_OUT=5*60*1000
 const WS_SERVER='http://127.0.0.1:7001'
-
+const CONNECT_TIME_OUT=5*1000
 class Cloudbuild {
   constructor(git, options) {
     this.git=git
     this.buildCmd=options.buildCmd
     this.timeout=TIME_OUT
   }
+
+  doTimeout(fn,timeout){
+    this.timer&&clearTimeout(this.timer)
+    log.info('设置任务超时时间：',`${timeout/1000}秒`)
+    this.timer=setTimeout(fn,timeout)
+  }
+
 
   //创建websocket链接
   init(){
@@ -22,19 +30,16 @@ class Cloudbuild {
     socket.on('connect', () => {
       console.log('connect!');
     });
+    const disconnect=()=>{
+      clearTimeout(this.timer)
+      socket.disconnect()
+      socket.close()
+    }
+    this.doTimeout(()=>{
+      log.error('云构建服务连接超时，自动终止')
+      disconnect()
+    },CONNECT_TIME_OUT)
   }
 }
 
-// const socket = require('socket.io-client')('http://127.0.0.1:7001');
-
-// socket.on('connect', () => {
-//   console.log('connect!');
-//   socket.emit('chat', 'hello world!');
-//   socket.emit('hello', '');
-//   socket.emit('welcome', '');
-// });
-
-// socket.on('res', msg => {
-//   console.log('res from server: %s!', msg);
-// });
 module.exports = Cloudbuild
