0bbafbd607a262e420520c59ff0a45ed263f11aa
haha58 <1461089222@qq.com>
2024-06-23 20:54:49 +0800

    服务端源码下载+切换到开发分支功能开发

--- packages/models/cloudbuild/lib/index.js
+++ packages/models/cloudbuild/lib/index.js
@@ -8,7 +8,6 @@ const TIME_OUT = 5 * 60 * 1000
 const WS_SERVER = 'http://127.0.0.1:7001'
 const CONNECT_TIME_OUT = 5 * 1000
 
-const FAILED_CODE=['prepare failed']
 
 function parseMsg(msg) {
   //msg 通过.的形式得到对象
@@ -35,75 +34,78 @@ class Cloudbuild {
 
   //创建websocket链接
   init() {
-    return new Promise((resolve,reject)=>{
-    const socket = io(WS_SERVER, {
-      query: {
-        repo: this.git.remote,
-        name:this.git.name,
-        branch:this.git.branch,
-        version:this.git.version,
-        buildCmd:this.git.buildCmd
-      }
-    });
-    socket.on('connect', () => {
-      this.timer && clearTimeout(this.timer)
-     /* const id = socket.id;
-        socket.on(id, msg => {
-        log('#receive,', msg);
-        const parsedMsg = parseMsg(msg);
-        log.success(parsedMsg.action, parsedMsg.message);
+    return new Promise((resolve, reject) => {
+      const socket = io(WS_SERVER, {
+        query: {
+          repo: this.git.remote,
+          name: this.git.name,
+          branch: this.git.branch,
+          version: this.git.version,
+          buildCmd: this.git.buildCmd
+        }
       });
-      */
-      resolve()
-    });
+      socket.on('connect', () => {
+        this.timer && clearTimeout(this.timer)
+        /* const id = socket.id;
+           socket.on(id, msg => {
+           log('#receive,', msg);
+           const parsedMsg = parseMsg(msg);
+           log.success(parsedMsg.action, parsedMsg.message);
+         });
+         */
+        resolve()
+      });
 
-    socket.on('id', msg => {
-      const parseedMsg = parseMsg(msg)
-      log.success('云构建任务创建成功', `任务ID：${parseedMsg.payload.id}`)
-      log.success(parseedMsg.action, parseedMsg.payload.message)
-    })
+      socket.on('id', msg => {
+        const parseedMsg = parseMsg(msg)
+        log.success('云构建任务创建成功', `任务ID：${parseedMsg.payload.id}`)
+        log.success(parseedMsg.action, parseedMsg.payload.message)
+      })
 
-    const disconnect = () => {
-      clearTimeout(this.timer)
-      socket.disconnect()
-      socket.close()
-    }
-    this.doTimeout(() => {
-      log.error('云构建服务连接超时，自动终止')
-      disconnect()
-    }, CONNECT_TIME_OUT)
+      const disconnect = () => {
+        clearTimeout(this.timer)
+        socket.disconnect()
+        socket.close()
+      }
+      this.doTimeout(() => {
+        log.error('云构建服务连接超时，自动终止')
+        disconnect()
+      }, CONNECT_TIME_OUT)
 
-    socket.on('disconnect', () => {
-      log.success('disconnect', '云构建任务断开')
-      disconnect()
+      socket.on('disconnect', () => {
+        log.success('disconnect', '云构建任务断开')
+        disconnect()
+      })
+      socket.on('error', () => {
+        log.error('error', '云构建出错', err)
+        reject(err)
+        disconnect()
+      })
+      this.socket = socket
     })
-    socket.on('error', () => {
-      log.error('error', '云构建出错', err)
-      reject(err)
-      disconnect()
-    })
-    this.socket=socket
-  })
   }
 
-  build(){
-    return new Promise((resolve,reject)=>{
+  build() {
+    return new Promise((resolve, reject) => {
       this.socket.emit('build')  //触发服务端build事件
-      this.socket.on('build',(msg)=>{  //监听服务端build事件
-          const parsedMsg=parseMsg(msg)
-          const action=parsedMsg.action
-          const message=parsedMsg.payload.message;
-          if(FAILED_CODE.indexOf(action)>=0){
-            log.error(action,message)
-            clearTimeout(this.timer)
-            this.socket.disconnect()
-            this.socket.close()
-          }else{
-            log.success(action, message)
-          }
-         
-        })
-        this.socket.on('building',(msg)=>{  //监听building-正在构建的事件
+      this.socket.on('build', (msg) => {  //监听服务端build事件
+        const parsedMsg = parseMsg(msg)
+        const action = parsedMsg.action
+        const message = parsedMsg.payload.message;
+        log.success(action, message)
+      })
+      this.socket.on('building', (msg) => {  //监听building-正在构建的事件
+      })
+      this.socket.on('buildError', (msg) => {  //监听服务端build事件
+        const parsedMsg = parseMsg(msg)
+        const action = parsedMsg.action
+        const message = parsedMsg.payload.message;
+        log.error(action, message)
+        clearTimeout(this.timer)
+        this.socket.disconnect()
+        this.socket.close()
+      })
+      this.socket.on('building', (msg) => {  //监听building-正在构建的事件
       })
     })
   }
